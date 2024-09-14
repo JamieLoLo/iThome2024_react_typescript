5351fc4f45d76bd0265da49aadc37e8a6c4b5576
JamieLoLo <bleach7408@hotmail.com>
2024-09-14 23:34:39 +0800

    【 Day 12 】Strict Mode

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Todo from './compnents/Todo'
+
+function App() {
+  return (
+    <main>
+      <Todo />
+    </main>
+  )
+}
+
+export default App
---
+++ src/compnents/Todo.tsx
@@ -0,0 +1,10 @@
+export default function Todo(props) {
+  return (
+    <div className='flex items-center gap-[20px]'>
+      <p>content</p>
+      <p>isFinished</p>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  )
+}
