bd9ef42868532e28526b7b2f75b79680bc13ec5c
JamieLoLo <bleach7408@hotmail.com>
2024-09-16 21:12:59 +0800

    【 Day 12 】Strict Mode

--- src/compnents/Todo.tsx
+++ src/compnents/Todo.tsx
@@ -1,8 +1,8 @@
 export default function Todo(props) {
   return (
     <div className='flex items-center gap-[20px]'>
-      <p>content</p>
-      <p>isFinished</p>
+      <input type='checkbox' checked={false} />
+      <p>Content</p>
       <button>Edit</button>
       <button>Delete</button>
     </div>
