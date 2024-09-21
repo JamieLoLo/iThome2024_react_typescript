44a6dc5dfa14be659a9416d0769ff2bc51be3b0d
JamieLoLo <bleach7408@hotmail.com>
2024-09-22 00:07:59 +0800

    【 Day 16 】使用 useState 儲存 Todo 項目

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
 import './App.css'
+import Header from './components/Header'
 import Todo from './components/Todo'
+import logo from './assets/logo.png'
+import { useState } from 'react'
+
+type Todo = {
+  id: number
+  title: string
+  isFinished: boolean
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const createTodoHandler = () => {
+    const newTodo: Todo = {
+      id: Math.random(),
+      title: 'Learn JavaScript',
+      isFinished: false,
+    }
+    setTodos((prevTodos) => [...prevTodos, newTodo])
+  }
+
   return (
-    <main>
-      <Todo isFinished={false} />
+    <main className='w-[500px] h-[100dvh] portrait:w-[90%] flex flex-col'>
+      <Header image={{ src: logo, alt: 'logo' }}>
+        <h1>Todo List</h1>
+      </Header>
+      <div className='w-full mb-[20px] h-fit flex'>
+        <label htmlFor='newTodo'></label>
+        <input
+          type='text'
+          id='newTodo'
+          name='newTodo'
+          className='flex-1 mr-[16px] px-3 rounded-[5px] focus:outline-none'
+        />
+        <button onClick={createTodoHandler}>Create</button>
+      </div>
+      {todos.map((todo) => (
+        <li key={todo.id} className='list-none'>
+          <Todo isFinished={todo.isFinished}>
+            <p>{todo.title}</p>
+          </Todo>
+        </li>
+      ))}
     </main>
   )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,16 @@
-import { FC } from 'react'
+import { type PropsWithChildren } from 'react'
 
-type TodoProps = { isFinished: boolean }
+type TodoProps = PropsWithChildren<{ isFinished: boolean }>
 
-const Todo: FC<TodoProps> = ({ isFinished }) => {
+export default function Todo({ isFinished, children }: TodoProps) {
   return (
-    <div className='flex items-center gap-[20px]'>
+    <div className='flex items-center gap-[20px] justify-between mb-3'>
       <input type='checkbox' checked={isFinished} />
-
-      <button>Edit</button>
-      <button>Delete</button>
+      {children}
+      <div className='flex gap-[16px]'>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   )
 }
-
-export default Todo
